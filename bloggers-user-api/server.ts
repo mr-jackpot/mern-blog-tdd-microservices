11971cdc167bdcb29aec4a34dcf346c40dbf3733
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-12-07 22:47:40 +0000

    basic express server set up

--- bloggers-user-api/server.ts
+++ bloggers-user-api/server.ts
@@ -3,6 +3,11 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.listen(`${port}/api/user`, () => {
-  console.log(`Timezones by location application is running on port ${port}.`);
+app.get(`/api/user`, (req, res) => {
+      res.send("Hello from user api!")
+    }
+  )
+
+app.listen(port, () => {
+  console.log(`Application is running on port ${port} 🚀.`);
 });
