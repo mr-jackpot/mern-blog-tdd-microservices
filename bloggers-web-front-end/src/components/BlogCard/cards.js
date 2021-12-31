68b6f8165ce604d62cefaef010e96f1043c8dd5a
Adam Heeps <adamheeps@adams-mbp.home>
2021-12-31 11:23:22 +0000

    saving progress for now, got a broken few buttons

--- bloggers-web-front-end/src/components/BlogCard/cards.js
+++ bloggers-web-front-end/src/components/BlogCard/cards.js
@@ -5,6 +5,7 @@ import './cards.css'
 const BlogCard = (props) => {
 
     const [blog, setBlog] = React.useState("");
+    const [isEditing, setEditing] = React.useState(false);
 
     const bo = {
         date: new Date().toISOString(),
@@ -24,6 +25,11 @@ const BlogCard = (props) => {
         window.location.reload()
     }
 
+    const prepareUpdate = (event) => {
+        event.preventDefault()
+        setEditing(true)
+    }
+
     const updatePost = (event) => {
         event.preventDefault()
         axios.put(`http://localhost:3005/api/blogs/${props.id}`, bo)
@@ -37,6 +43,25 @@ const BlogCard = (props) => {
         window.location.reload()
     }
 
+    const cancelUpdate = (event) => {
+        event.preventDefault()
+        this.setEditing(false)
+    }
+
+    const SubmitUpdate = () => {
+        if (isEditing) {
+            return <div>
+                    <button onClick={updatePost}> Submit </button>
+                    <button onClick={cancelUpdate}> Cancel </button>
+                </div>
+        } else {
+            return <div>
+                    <button id="update-post-button" onClick={prepareUpdate} data-cy="update-post-button"> Update Post</button>
+                </div>
+        }
+
+    }
+
     if (props.user !== null) {
     return (
         <div className="blog-container" data-cy="blog-container">
@@ -47,7 +72,7 @@ const BlogCard = (props) => {
                     <button id="delete-post-button" onClick={deletePost} data-cy="delete-post-button"> Remove</button>
                 </div>
                 <div className="header-update">
-                    <button id="update-post-button" onClick={updatePost} data-cy="update-post-button"> Update Post</button>
+                    <SubmitUpdate />
                 </div>
             </div>
             <div className="container-body">
