d83aec01c1b2e36eac7696fad8f00dbc1f2441a9
Adam Heeps <adamheeps@adams-mbp.home>
2021-12-31 10:42:04 +0000

    started implementing update function

--- bloggers-web-front-end/src/components/BlogCard/cards.js
+++ bloggers-web-front-end/src/components/BlogCard/cards.js
@@ -4,18 +4,37 @@ import './cards.css'
 
 const BlogCard = (props) => {
 
-    const handleClick = (event) => {
-      event.preventDefault()
-      deletePost()
-      window.location.reload()
+    const [blog, setBlog] = React.useState("");
+
+    const bo = {
+        date: new Date().toISOString(),
+        blog: blog
     }
 
-    const deletePost = () => {
-      axios.delete(`http://localhost:3005/api/blogs/${props.id}`)
+    const deletePost = (event) => {
+        event.preventDefault()
+        axios.delete(`http://localhost:3005/api/blogs/${props.id}`)
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+        window.location.reload()
     }
 
-    const updatePost = () => {
-      
+    const updatePost = (event) => {
+        event.preventDefault()
+        axios.put(`http://localhost:3005/api/blogs/${props.id}`, bo)
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+        window.location.reload()
     }
 
     if (props.user !== null) {
@@ -25,7 +44,10 @@ const BlogCard = (props) => {
                 <div className="header-user"> {props.user} </div>
                 <div className="header-date"> {props.date} </div>
                 <div className="header-delete"> 
-                    <button id="delete-post-button" onClick={handleClick} data-cy="delete-post-button"> Remove Post </button>
+                    <button id="delete-post-button" onClick={deletePost} data-cy="delete-post-button"> Remove</button>
+                </div>
+                <div className="header-update">
+                    <button id="update-post-button" onClick={updatePost} data-cy="update-post-button"> Update Post</button>
                 </div>
             </div>
             <div className="container-body">
