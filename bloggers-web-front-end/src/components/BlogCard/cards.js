68e0b535f78b1a2892fde8be065b60316c0c0985
Adam Heeps <adamheeps@jacquelplewatch.home>
2022-01-02 15:40:02 +0000

    lost in the sauce atm. want this form to render on clikc

--- bloggers-web-front-end/src/components/BlogCard/cards.js
+++ bloggers-web-front-end/src/components/BlogCard/cards.js
@@ -4,12 +4,32 @@ import './cards.css'
 
 const BlogCard = (props) => {
 
-    const [blog, setBlog] = React.useState("");
-    const [isEditing, setEditing] = React.useState(false);
+    const [blog, setBlog] = React.useState(props.blog)
+    const [showBlog, setShowBlog] = React.useState(false)
 
     const bo = {
         date: new Date().toISOString(),
         blog: blog
+    }
+
+    const handleClick = (event) => {
+        event.preventDefault()
+        setShowBlog(true)
+    }
+
+    const SubmitBlogForm = () => {
+      
+        if (!showBlog) {
+            return (
+            <div>
+                <form>
+                    <textarea onChange={(b) => setBlog(b.target.value)} value={blog}></textarea>
+                    <submit type="submit" value="Post Blog"></submit>
+                </form>
+            </div>
+            )} else {
+                return (<div>RETURN FIRING</div>)
+        }
     }
 
     const deletePost = (event) => {
@@ -25,11 +45,6 @@ const BlogCard = (props) => {
         window.location.reload()
     }
 
-    const prepareUpdate = (event) => {
-        event.preventDefault()
-        setEditing(true)
-    }
-
     const updatePost = (event) => {
         event.preventDefault()
         axios.put(`http://localhost:3005/api/blogs/${props.id}`, bo)
@@ -43,25 +58,6 @@ const BlogCard = (props) => {
         window.location.reload()
     }
 
-    const cancelUpdate = (event) => {
-        event.preventDefault()
-        this.setEditing(false)
-    }
-
-    const SubmitUpdate = () => {
-        if (isEditing) {
-            return <div>
-                    <button onClick={updatePost}> Submit </button>
-                    <button onClick={cancelUpdate}> Cancel </button>
-                </div>
-        } else {
-            return <div>
-                    <button id="update-post-button" onClick={prepareUpdate} data-cy="update-post-button"> Update Post</button>
-                </div>
-        }
-
-    }
-
     if (props.user !== null) {
     return (
         <div className="blog-container" data-cy="blog-container">
@@ -72,11 +68,16 @@ const BlogCard = (props) => {
                     <button id="delete-post-button" onClick={deletePost} data-cy="delete-post-button"> Remove</button>
                 </div>
                 <div className="header-update">
-                    <SubmitUpdate />
+                    <form>
+                        <button id="update-post-button" onClick={handleClick}> Update </button>
+                    </form>
                 </div>
             </div>
             <div className="container-body">
-                <div className="body-blog"> {props.blog} </div>
+                <div className="body-blog"> 
+                    {blog}
+                </div>
+                <SubmitBlogForm />
             </div>
         </div>
     )}
